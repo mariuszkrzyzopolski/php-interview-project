6325f2e6fc90959c62ffc11b83ec5e0488514e9a
Mariusz Krzyzopolski <krzyzopolski.mariusz@gmail.com>
2019-03-20 12:40:28 +0100

    seperate database login data

--- koni.php
+++ koni.php
@@ -1,17 +1,17 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "koni";
-echo $_POST['status']."<br>";
+//import and start connection to the database
+require "connect.php";
 $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$query = "INSERT INTO Zadania (name,surname,company,email,problem,status)
-VALUES ('".$_POST["name"]."', '".$_POST["surname"]."','".$_POST["company"]."', '".$_POST["mail"]."','".$_POST["problem"]."','".$_POST["status"]."')";
-echo $query;
+//create adding query
+$query = "INSERT INTO 
+Zadania (name,surname,company,email,problem,status)
+VALUES 
+('".$_POST["name"]."', '".$_POST["surname"]."','".$_POST["company"]."', '".$_POST["mail"]."','".$_POST["problem"]."','".$_POST["status"]."')";
+
 $conn->exec($query);
 if($conn){
-    echo "New record created successfully";
+    echo "success";
 }
 else{
     echo $query;
